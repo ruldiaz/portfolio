c33ae013bd587d1cfea29d0ea477b463bb398d5b
RaulDiaz <rulhdiaz@gmail.com>
2023-05-26 11:28:38 -0600

    components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,7 @@
 import './App.css'
+import Footer from './components/footer'
+import Header from './components/header'
+import Projects from './components/projects'
 
 function App() {
 
@@ -6,6 +9,9 @@ function App() {
   return (
     <>
       <h1>Portfolio of Raul in Construction</h1>
+      <Header />
+      <Projects />
+      <Footer />
     </>
   )
 }
